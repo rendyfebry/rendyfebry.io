f15fa4d551e05d20c26e8209e19d1677e72c258e
Rendy Febry <rendyfebry@gmail.com>
2020-04-15 18:27:11 +0700

    Add Analytics

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import VueHighcharts from 'vue-highcharts'
+import VueAnalytics from 'vue-analytics';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -12,6 +13,11 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+	id: 'UA-163693676-1',
+	router,
+})
 
 Vue.use(BootstrapVue)
 Vue.use(VueHighcharts)
